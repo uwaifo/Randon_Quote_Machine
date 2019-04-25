2eddd67f76c8d42372c1d3db0eda7d3feec71094
uwaifo <mr.uwaifo@gmail.com>
2019-04-25 17:58:26 +0100

    First Draft

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "@material-ui/core/Link";
+
+export default function Footer() {
+  return (
+    <div>
+      <Link href="http://www.overstandapps.com" variant="body1">
+        Designed by OverstandApps
+      </Link>
+    </div>
+  );
+}
